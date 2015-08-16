251aecf81c98beb130f419219b39eb54561cae6d
oimou <s1190013@gmail.com>
2015-08-16 21:37:30 +0900

    test: Separate lines into each module

--- test/index.js
+++ test/index.js
@@ -14,43 +14,67 @@ describe("OPC", function () {
         opc.host = "localhost";
     });
 
-    it("should send HTTP commands with specified headers", function () {
-        opc._request = function (opt) {
-            expect(opt).to.be.an("object");
-            expect(opt.headers).to.be.an("object");
-            expect(opt.headers["User-Agent"]).to.equal("OlympusCameraKit");
-        };
-
-        opc.getConnectmode(nop);
-    });
-
-    it("should start an event server on calling startPushevent", function (done) {
-        var expectedEventServerPort = 65000;
-
-        server = net.createServer();
-
-        server.listen(expectedEventServerPort, function () {
-            opc._request = function (opt, cb) {
-                cb(null, { statusCode: 200 }, "");
-            };
-
-            var param = {
-                port: expectedEventServerPort
-            };
-
-            expect(opc.eventServer).to.not.exist;
-
-            opc.startPushevent(param, function () {
-                expect(opc.eventServer).to.exist;
-                expect(opc.eventServerPort).to.equal(expectedEventServerPort);
-
-                done();
-            });
-        });
-    });
-
     afterEach(function () {
         opc && opc.stopPushevent();
         server && server.close();
+    });
+
+    /*
+     *  lib/core
+     */
+    describe("core module", function () {
+        xit("#destroy should close connections to camera", function () {
+        });
+    });
+
+    /*
+     *  lib/ble
+     */
+    describe("ble module", function () {
+    });
+
+    /*
+     *  lib/command
+     */
+    describe("command module", function () {
+        it("#someCommand should send HTTP commands with specified headers", function () {
+            opc._request = function (opt) {
+                expect(opt).to.be.an("object");
+                expect(opt.headers).to.be.an("object");
+                expect(opt.headers["User-Agent"]).to.equal("OlympusCameraKit");
+            };
+
+            opc.getConnectmode(nop);
+        });
+    });
+
+    /*
+     *  lib/event
+     */
+    describe("event module", function () {
+        it("#startPushevent should start an event server", function (done) {
+            var expectedEventServerPort = 65000;
+
+            server = net.createServer();
+
+            server.listen(expectedEventServerPort, function () {
+                opc._request = function (opt, cb) {
+                    cb(null, { statusCode: 200 }, "");
+                };
+
+                var param = {
+                    port: expectedEventServerPort
+                };
+
+                expect(opc.eventServer).to.not.exist;
+
+                opc.startPushevent(param, function () {
+                    expect(opc.eventServer).to.exist;
+                    expect(opc.eventServerPort).to.equal(expectedEventServerPort);
+
+                    done();
+                });
+            });
+        });
     });
 });
