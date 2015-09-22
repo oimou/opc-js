5bcc9798ba99b7417e05a40a5d293006852a7088
oimou <s1190013@gmail.com>
2015-09-22 15:22:50 +0900

    Implement lightbox

---
+++ index.js
@@ -0,0 +1,20 @@
+/*globals $:true */
+
+"use strict";
+
+$(function() {
+    $("[data-popup]").magnificPopup({
+        type: "image",
+        closeOnContentClick: true,
+        closeBtnInside: false,
+        fixedContentPos: true,
+        mainClass: "mfp-no-margins mfp-with-zoom",
+        image: {
+            verticalFit: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300
+        }
+    });
+});
